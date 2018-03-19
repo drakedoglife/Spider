a52319f391e9f225ac4c8f9d31bc1029beab2bca
gaoyifang <drakedoglife@outlook.com>
2018-03-19 19:40:41 +0800

    finish get page

--- spider.py
+++ spider.py
@@ -1,4 +1,6 @@
+import re
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,6 +9,21 @@ driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 10)
 
 def get_page():
+    driver.get("https://www.taobao.com")
     try:
         input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#q')))
-        submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '')))
+        submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_TSearchForm > div.search-button > button')))
+        input.send_keys('美食')
+        submit.click()
+        total = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > div.total')))
+        return total.text
+    except TimeoutException:
+        return get_page()
+
+def main():
+    total = re.compile('\d+').search(get_page()).group()
+    print(total)
+
+if __name__ == '__main__':
+    main()
+
